399d63dd4f2c97df3c13b85886f3402b2039752a
Kilian Pouliquen <kipouliq@made-f0Br6s1.clusters.42paris.fr>
2023-11-30 18:12:31 +0100

    works good need more refactoring

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 17:20:39 by kipouliq          #+#    #+#             */
-/*   Updated: 2023/11/29 19:04:48 by kipouliq         ###   ########.fr       */
+/*   Updated: 2023/11/30 18:05:17 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
+
+char	*something_happened(char *s1, char *s2)
+{
+	if (s1)
+	{
+		free(s1);
+		s1 = NULL;
+	}
+	if (s2)
+	{
+		free(s2);
+		s2 = NULL;
+	}
+	return (NULL);
+}
 
 size_t	ft_strlen(const char *s)
 {
@@ -51,26 +66,13 @@ char	*get_nbytes(int fd, int *bytes_read)
 	int		i;
 
 	i = -1;
-	str = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	str = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // ok
 	if (!str)
-		return (NULL);
+		return (something_happened(str, NULL));
 	while (++i < BUFFER_SIZE + 1)
 		str[i] = '\0';
 	*bytes_read = read(fd, str, BUFFER_SIZE);
 	return (str);
-}
-
-int	get_str_size(char *str, char *end_ptr)
-{
-	int	i;
-
-	i = 0;
-	while (str != end_ptr)
-	{
-		str++;
-		i++;
-	}
-	return (i);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
@@ -85,9 +87,9 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (s1);
 	s1_size = ft_strlen(s1);
 	s2_size = ft_strlen(s2);
-	final_str = malloc(sizeof(char) * (s1_size + s2_size) + 1);
+	final_str = malloc(sizeof(char) * (s1_size + s2_size + 1)); // ok
 	if (!final_str)
-		return (NULL);
+		return (something_happened(s1, s2));
 	ft_strlcpy(final_str, s1, s1_size + 1);
 	ft_strlcpy(final_str + s1_size, s2, s2_size + 1);
 	free(s1);
@@ -95,32 +97,15 @@ char	*ft_strjoin(char *s1, char *s2)
 	return (final_str);
 }
 
-char	*ft_strchr(char *s, int c)
+char	*ft_strcpy_malloc(char *src, char *end_ptr)
 {
-	int	i;
-
-	i = -1;
-	if (!s)
-		return (NULL);
-	while (s[++i])
-	{
-		if (s[i] == (char)c)
-			return ((char *)s + i);
-	}
-	if (c == '\0')
-		return ((char *)s + i);
-	return (NULL);
-}
-
-char    *ft_strcpy_malloc(char *src, char *end_ptr)
-{
-	char *final_str;
-	int   i;
+	char	*final_str;
+	int		i;
 
 	i = 0;
-	final_str = malloc(sizeof(char) * get_str_size(src, end_ptr) + 1); // test + modif si necessaire
+	final_str = malloc(sizeof(char) * (end_ptr - src + 1)); // ok
 	if (!final_str)
-		return (NULL);
+		return (something_happened(src, NULL));
 	while (src + i != end_ptr)
 	{
 		final_str[i] = src[i];
@@ -130,86 +115,75 @@ char    *ft_strcpy_malloc(char *src, char *end_ptr)
 	return (final_str);
 }
 
-void    ft_bzero(void *s, size_t n)
+char	*ft_dup_malloc_free(char *str, char *to_free)
 {
-	size_t  i;
+	char	*final_str;
+	int		i;
 
 	i = -1;
-	while (++i < n)
-		*((char *)s + i) = '\0';
-}
-
-char    *ft_dup_malloc_free(char *str, char *to_free)
-{
-	char *final_str;
-	int   i;
-	
-	i = -1;
-	final_str = malloc(sizeof(char) * ft_strlen(str) + 1);
+	final_str = malloc(sizeof(char) * ft_strlen(str) + 1); // ok
 	if (!final_str)
-		return (NULL);
+		return (something_happened(final_str, to_free));
 	while (str[++i])
 		final_str[i] = str[i];
 	final_str[i] = '\0';
 	if (to_free)
 		free(to_free);
-	// printf("final str = %s\n", final_str);
-	return (final_str);   
+	return (final_str);
 }
 
-// char	*get_next_line_old(int fd)
-// {
-// 	static char	*static_str;
-// 	char		*line;
-// 	char		*eol;
-// 	int			bytes_read;
-
-// 	bytes_read = 1;
-// 	while (bytes_read)
-// 	{
-// 		if (!static_str && !line)
-// 			break ;
-// 		if (ft_strchr(static_str, '\0') && !bytes_read)
-// 		{
-// 			free(line);
-// 			return (static_str);
-// 		}
-// 		eol = ft_strchr(static_str, '\n');
-// 		if (eol)
-// 		{
-// 			line = ft_strcpy_malloc(static_str, eol + 1);
-// 			static_str = ft_dup_malloc_free(eol + 1, static_str);
-// 			if (!static_str || !line)
-// 				return (NULL);
-// 			return (line);
-// 		}
-// 		line = get_nbytes(fd, &bytes_read);
-// 		if (!line)
-// 			return (NULL);
-// 		static_str = ft_strjoin(static_str, line);
-// 		if (!static_str)
-// 			return (NULL);
-// 		if (bytes_read != BUFFER_SIZE)
-// 		{
-// 			line = ft_dup_malloc_free(static_str, static_str);
-// 			if (!line)
-// 				return (NULL);
-// 			return (line);
-// 		}
-// 	}
-// 	if (ft_strchr(static_str, '\0') && !bytes_read)
-// 		return (static_str);
-// 	return (NULL);
-// }
-
-int		check_static_str(char *static_str, char **eol)
+int	check_static_str(char *static_str, char **eol)
 {
-	*eol = ft_strchr(static_str, '\n');
+	int	i;
+
+	i = -1;
+	*eol = NULL;
+	if (!static_str)
+		return (0);
+	while (static_str[++i])
+	{
+		if (static_str[i] == '\n')
+		{
+			*eol = static_str + i;
+			break ;
+		}
+	}
 	if (*eol)
 		return (1);
 	else
-		return (0); 
-	
+		return (0);
+}
+
+char	*ft_end_of_file(char **line, char **static_str)
+{
+	free(*line);
+	*line = ft_dup_malloc_free(*static_str, *static_str);
+	if (!line)
+		return (NULL);
+	*static_str = NULL;
+	return (*line);
+}
+
+char	*ft_fill_static(char **static_str, char **eol, int *bytes_read, int fd)
+{
+	char	*line;
+
+	// line = malloc(1);
+	// if (!line)
+	// return (something_happened(line, NULL));
+	while (!check_static_str(*static_str, eol))
+	{
+		// free(line);
+		line = get_nbytes(fd, bytes_read);
+		if (!line)
+			return (NULL);
+		if (!(*bytes_read))
+			break ;
+		*static_str = ft_strjoin(*static_str, line);
+		if (!(*static_str))
+			return (NULL);
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
@@ -219,51 +193,45 @@ char	*get_next_line(int fd)
 	char		*eol;
 	int			bytes_read;
 
-	eol = NULL;
-	line = get_nbytes(fd, &bytes_read);
-	if (!bytes_read)
-		return (NULL);
-	while (!check_static_str(static_str, &eol))
+    // eol = NULL;
+	if (!check_static_str(static_str, &eol))
 	{
-		line = get_nbytes(fd, &bytes_read);
+		line = ft_fill_static(&static_str, &eol, &bytes_read, fd);
 		if (!line)
-			return (NULL);
-		if (!bytes_read)
-			break ;
-		static_str = ft_strjoin(static_str, line);
-		if (!static_str)
-			return (NULL);
+			return (something_happened(static_str, NULL));
 	}
 	if (eol)
 	{
 		line = ft_strcpy_malloc(static_str, eol + 1);
-		static_str = ft_dup_malloc_free(eol + 1, static_str);
-		if (!static_str || !line)
+		if (!line)
 			return (NULL);
+		static_str = ft_dup_malloc_free(eol + 1, static_str);
+		if (!static_str)
+			return (something_happened(line, NULL));
 		return (line);
 	}
-	if (!bytes_read)
-	{
-		line = ft_dup_malloc_free(static_str, static_str);
-		static_str = NULL;
-		return (line);
-	}
+	if (!bytes_read && static_str)
+		return (ft_end_of_file(&line, &static_str));
+	if (line)
+		free(line);
 	return (NULL);
 }
 
 int	main(void)
 {
-	char *str;
-	int fd = open("./test.txt", O_RDONLY);
-	int i = 0;
+	char	*str;
+	int		fd;
 
-	while (i < 5)
+	fd = open("./test.txt", O_RDONLY);
+	while (1)
 	{
 		str = get_next_line(fd);
 		if (!str)
+		{
+			free(str);
 			break ;
+		}
 		printf("str = %s\n", str);
 		free(str);
-		i++;
 	}
 }
